81b5e47a584e504f7141367bac042eca65fe0be0
gitgamal <131804251+gitgamal@users.noreply.github.com>
2023-04-25 19:52:18 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,46 @@
-import requests
-import openai
-openai.api_key = "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"
-openai.organization = "org-XXX"
-def get_web_results(query):
- response = requests.get(f"https://api.google.com/search?q={query}")
- data = response.json()
- results = data["items"]
- return results
-def generate_response(prompt):
- completion = openai.ChatCompletion.create(
- model="gpt-3.5-turbo",
- messages=[
- {"role": "user", "content": prompt}
- ]
- )
- message = completion.choices[0].message.content.strip()
- return message
-query = input("Enter your search query: ")
-results = get_web_results(query)
-prompt = f"What do you want to know about {query}? Here are the top web results:\n"
-for result in results:
- prompt += f"- {result['title']}: {result['snippet']}\n"
-response = generate_response(prompt)
-print(response)
+from selenium import webdriver
+from selenium_stealth import stealth
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+
+
+def GetTop1GoogleResults(input):
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option('useAutomationExtension', False)
+    driver = webdriver.Chrome(
+        options=options, executable_path="\chromedriver.exe")
+
+    stealth(driver,
+            languages=["en-US", "en"],
+            vendor="Google Inc.",
+            platform="Win32",
+            webgl_vendor="Intel Inc.",
+            renderer="Intel Iris OpenGL Engine",
+            fix_hairline=True,
+            )
+
+    query = input
+
+    n_pages = 10
+    results = []
+    counter = 0
+    for page in range(1, n_pages):
+        url = "http://www.google.com/search?q=" + \
+            query + "&start=" + str((page - 1) * 10)
+        driver.get(url)
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        search = soup.find_all('div', class_="yuRUbf")
+        for h in search:
+            counter = counter + 1
+            title = h.a.h3.text
+            link = h.a.get('href')
+            rank = counter
+            results.append({'title': h.a.h3.text, 'url': link,
+                            'domain': urlparse(link).netloc, 'rank': rank})
+    return results[0]['url']
+    # for i, result in enumerate(results):
+    #    firstResult  = result['url']
+    #    print(
+    #        f"Result {i+1}: {result['title']}\n{result['url']}\n{result['domain']}\n{result['rank']}")
